c6809dcdbdd5389898f1f2148f3969698ee1e605
Дмитро Катюха <Dmytro.Katyukha@jbm.com.ua>
2014-10-09 19:18:36 +0300

    Added extension field_datetime.
    Automaticaly converts date and datetime fields to datetime related
    objects.

---
+++ openerp_proxy/ext/field_datetime.py
@@ -0,0 +1,17 @@
+from openerp_proxy.orm.record import Record
+
+import datetime
+
+
+class RecordDateTime(Record):
+    """ Provides auto conversion of datetime fields from
+        string got from server to comparable datetime objects
+    """
+
+    def _get_field(self, ftype, name):
+        res = super(RecordDateTime, self)._get_field(ftype, name)
+        if res and ftype == 'date':
+            return datetime.datetime.strptime(res, '%Y-%m-%d').date()
+        elif res and ftype == 'datetime':
+            return datetime.datetime.strptime(res, '%Y-%m-%d %H:%M:%S')
+        return res
