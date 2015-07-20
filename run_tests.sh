26eee40fc0a9aede96494eda8f32392cd1c85630
Дмитро Катюха <Dmytro.Katyukha@jbm.com.ua>
2015-07-20 19:09:56 +0300

    Revert "Start using nose for tests"
    This reverts commit 323282b64ede0154e87442d77b9291aebf733b4c.

--- run_tests.sh
+++ run_tests.sh
@@ -4,4 +4,4 @@ SCRIPT=`readlink -f "$0"`
 # Absolute path this script is in, thus /home/user/bin
 SCRIPTPATH=`dirname "$SCRIPT"`
 
-(cd $SCRIPTPATH && nosetests --with-coverage --cover-html --cover-html-dir=coverage --cover-package=openerp_proxy.* -v )
+(cd $SCRIPTPATH && coverage run --source openerp_proxy -m unittest discover -v && coverage html -d coverage)
